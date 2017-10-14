cef1fb25e78d791dd15dd396299ae2940e5856cd
blanklist <joehineriv@gmail.com>
2017-10-14 16:26:11 -0700

    chapter 1 complete

--- add_items.cpp
+++ add_items.cpp
@@ -2,8 +2,20 @@
 #include "Sales_item.h"
 int main()
 {
-	Sales_item item1, item2;
-	std::cin >> item1 >> item2;
-	std::cout << item1 + item2 << std::endl;
-	return 0;
+	Sales_item total;
+	if (std::cin >> total) {
+		Sales_item trans;
+		while (std::cin >> trans) {
+			if (total.isbn() == trans.isbn())
+				total += trans;
+			else {
+				std::cout << total << std::endl;
+				total = trans;
+			}
+		}
+		std::cout << total << std::endl;
+	} else {
+		std::cerr << "No data?!" << std::endl;
+		return -1;
+	}
 }
